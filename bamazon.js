af54eab5e9dd2a3bc79f1bd360571f9e094bcf88
emily599 <emilydoan52@gmail.com>
2021-01-06 19:48:56 -0800

    Fix connection issue

--- bamazon.js
+++ bamazon.js
@@ -5,8 +5,6 @@ var cTable = require("console.table");
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user: "root",
-    password: "Cupcake123!",
     database: "bamazon_DB"
 });
 
